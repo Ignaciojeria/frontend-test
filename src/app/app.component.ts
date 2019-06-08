77104e39f403e53a4a854552f1018ff5737b32d7
Ignacio <you@example.com>
2019-06-08 11:58:20 -0400

    game is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GameService} from './game.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+
+  public step: number;
+
+  public isInGame: boolean;
+
+
+  public doors: Array<any>;
+
+  constructor(private gameService: GameService) {
+
+  }
+
+  ngOnInit(): void {
+    this.step = 1;
+  }
+
+  public play(door?: any) {
+    if (door === undefined || door.gift === undefined) {
+      switch (this.step) {
+        case 1: {
+          this.playTheFirstStep();
+          this.step++;
+          break;
+        }
+        case 2: {
+          this.playTheSecondtStep(door);
+          this.step++;
+          break;
+        }
+        case 3: {
+          if (door.gift === undefined) {
+            this.playThirdStep(door);
+            this.step = 1;
+          }
+          break;
+        }
+      }
+    }
+
+  }
+
+
+  private playTheFirstStep() {
+    this.gameService.createNewGame().subscribe((data: any) => {
+      this.doors = data.doors;
+      this.doors.forEach(item => {
+        item.url = 'assets/DOOR.png';
+      });
+
+      this.isInGame = true;
+    });
+  }
+
+
+  private playTheSecondtStep(door: any) {
+    this.gameService.chooseDoorAndGiveOpportunity(door).subscribe((data: any) => {
+
+      this.doors = data;
+
+      this.setResourceImgUrl(this.doors);
+
+      this.sortById(this.doors);
+
+    });
+
+  }
+
+
+  private sortById(items) {
+    items.sort((a, b) => {
+      if (a.id > b.id) {
+        return 1;
+      } else if (a.id < b.id) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  private setResourceImgUrl(elements) {
+    elements.forEach(item => {
+      if (item.gift === 'GOAT') {
+        item.url = 'assets/CABRA.jpg';
+      } else if (item.gift === 'DOOR') {
+        item.url = 'assets/DOOR.png';
+      } else if (item.gift === 'CAR') {
+        item.url = 'assets/AUTO.jpg';
+      } else {
+        item.url = 'assets/DOOR.png';
+      }
+    });
+  }
+
+  private playThirdStep(door: any) {
+    this.gameService.chooseDoorAfterOpportunityWasGiven(door).subscribe((data: any) => {
+
+      this.doors = data[0];
+
+      this.setResourceImgUrl(this.doors);
+
+      this.sortById(this.doors);
+
+    });
+  }
+
+
 }
---
+++ src/app/game.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable()
+export class GameService {
+
+  private static readonly domain = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public createNewGame(): Observable<any> {
+    return this.http.post<any>(GameService.domain + '/createNewGame', null, httpOptions);
+  }
+
+  public chooseDoorAndGiveOpportunity(chooseDoorRequest: any): Observable<any> {
+    return this.http.post<any>(GameService.domain + '/chooseDoorAndGiveOpportunity', chooseDoorRequest, httpOptions);
+  }
+
+  public chooseDoorAfterOpportunityWasGiven(chooseDoorRequest: any): Observable<any> {
+    return this.http.post<any>(GameService.domain + '/chooseDoorAfterOpportunityWasGiven', chooseDoorRequest, httpOptions);
+  }
+
+}
